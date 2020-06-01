cffdf25da1f912b6fb3230b8d431e447cad634fe
AbrarChowdhury <abrar.chowdhury99@gmail.com>
2020-06-01 13:55:36 +0600

    create nav component & make homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/navbar/navbar.component'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <section className="home">
+        <div className="half-container">
+          <h2 className="name">Hi, I'm Abrar.</h2>
+          <h1 className="header"> Full-Stack Developer.</h1>
+          <p className="subtitle">The jack of all trades and Master of Web Development.<br/>A designer who loves diving deep into problem solving.<br/>An engineer by heart.
+          </p>
+          <p><span className="c2action">Learn more</span></p>  
+        </div>
+      </section>
+      
+      
+      <section className="about">
+        <div className="half-container">
+          <h2 className="name">Hi, I'm Abrar.</h2>
+          <h1 className="header">I build products with passion.</h1>
+          <p className="subtitle">The jack of all trades and Master of Web Development.<br/>A designer who loves diving deep into problem solving.<br/>An engineer by heart.</p>  
+        </div>      
+      </section>
+      
+      
+      <section className="home">
+      
+      </section>
+      
+      
+      <section className="home">
+      
+      </section>
     </div>
   );
 }
---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './navbar.style.css'
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        
+    )
+  }
+export default Navbar;
