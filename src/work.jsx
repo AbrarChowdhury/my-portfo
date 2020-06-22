d2e5edeeaf1c68d9cbe3cdc9143c8929ff52b83f
AbrarChowdhury <abrar.chowdhury99@gmail.com>
2020-06-22 17:49:30 +0600

    animation fix

--- src/work.jsx
+++ src/work.jsx
@@ -3,17 +3,17 @@ import ProjectCard from './components/projectCard/projectCard.component'
 import projectsArray from './projectsData';
 import { Link } from 'react-router-dom'
 import '../node_modules/aos/dist/aos.css'; 
-import AOSw from 'aos'; 
+import AOS from 'aos'; 
 class Work extends React.Component { 
   constructor(props, context) { 
     super(props, context); 
-    AOSw.init(); 
+    AOS.init(); 
   }
   componentDidMount (){ 
-    AOSw.refresh(); 
+    AOS.refresh(); 
   }  
   componentWillReceiveProps (){ 
-    AOSw.refresh(); 
+    AOS.refresh(); 
   } 
   render(){ 
     return (
@@ -23,10 +23,14 @@ class Work extends React.Component {
         <h1 className="header">View Some Of My Work.</h1>
         <h2 className="subtitle">Or, if you are already excited about building your project with me,</h2>
         <Link to="/contact"><p className="c2action">Let me know the details <i class="fa fa-chevron-right" aria-hidden="true"></i></p></Link>
+        <br/><br/><br/>
         <div className="projects">
         {projectsArray.map((project)=>(
-          <div className="aos-item"  data-aos-mirror="true" data-aos="zoom-in-left" data-aos-duration="1000" >
-            <ProjectCard title={project.title} desc={project.desc} image={project.image}link={project.link}/>
+          <div>
+            <div className="aos-item"  data-aos-mirror="true" data-aos="zoom-in-left" data-aos-duration="1000" >
+              <ProjectCard title={project.title} desc={project.desc} image={project.image}link={project.link}/>
+            </div>
+            <div className="border-bottom"></div>
           </div>
         ))}
      </div>
