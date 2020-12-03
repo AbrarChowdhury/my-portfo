ff797ed8170ec2492a44ea978512f983b715d6e6
abrar shams chowdhury <abrar.chowdhury99@gmail.com>
2020-12-03 14:46:19 +0600

    grammer fixed

--- src/home.jsx
+++ src/home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="half-container">
           <h2 className="name">Hi, I'm Abrar.</h2>
           <h1 className="header"> Full-Stack Developer.</h1>
-          <p className="subtitle">Jack of all trades, mastering of web development. An engineer by heart, I love to invent, design, analyze, build, and test.
+          <p className="subtitle">Jack of all trades, mastering web development. An engineer by heart, I love to invent, design, analyze, build, and test.
           </p>
           <Link to="/work">
           <p><span className="c2action">View my work <i class="fa fa-chevron-right" aria-hidden="true"></i></span></p>
